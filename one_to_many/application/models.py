19b206825f989ff5980d3bb8606597f021a2784f
lucaxgomex <lucaxgomex@gmail.com>
2023-08-07 17:49:54 -0300

    Adicionada tabela de entregadores

--- one_to_many/application/models.py
+++ one_to_many/application/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import Model
 
 # Endereco
 class Endereco(models.Model):
@@ -13,6 +12,32 @@ class Endereco(models.Model):
   def __str__(self):
     return self.rua
 
+# Entregador 
+class Entregador(models.Model):
+  SEXO_CHOICES = (
+    ("F", "Feminino"),
+    ("M", "Masculino"),
+    ("N", "Nenhuma das opções")
+  )
+
+  """
+  pedido = models.ForeignKey(
+    'Pedido', 
+    on_delete=models.CASCADE, 
+    related_name='pedidos'
+  )
+  """
+
+  nome = models.CharField(max_length=100, null=False, blank=False)
+  data_nascimento = models.DateField(null=False, blank=False)
+  email = models.EmailField(null=False, blank=False)
+  profissao = models.CharField(max_length=50, null=False, blank=False)
+  sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, blank=False, null=False)
+  cpf = models.IntegerField(null=False, blank=False)
+  
+  def __str__(self):
+    return self.nome
+    
 # Cliente
 class Cliente(models.Model):
   SEXO_CHOICES = (
@@ -48,6 +73,14 @@ class Pedido(models.Model):
     on_delete=models.CASCADE, 
     related_name='pedidos'
   )
+
+  
+  entregador = models.ForeignKey(
+    'Entregador', 
+    on_delete=models.CASCADE, 
+    related_name='entregadores'
+  )
+  
 
   observacoes = models.CharField(max_length=300, null=False, blank=False)
   data_pedido = models.DateTimeField()
